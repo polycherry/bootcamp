d9cdfa20d51f55ae49a40134f5e033cb5be61fba
polycherry <vasishtasetty@yahoo.co.in>
2024-10-07 10:51:31 +0100

    added package

---
+++ hello_package/main.py
@@ -0,0 +1,13 @@
+def complex_analysis(data, offset=1):
+    """Perform a complex analysis on the data and return the result
+
+    Args:
+        data (array): The data to analyze
+        offset (int): An offset to add to the data
+
+    Returns:
+        array: The result of the analysis
+    """
+
+    result = data + offset
+    return result
